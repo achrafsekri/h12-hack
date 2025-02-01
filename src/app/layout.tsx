f6288eb655a5f361707808981af47a8e78288984
Mohamed Ashref Ben Abdallah <109633107+Ashref-dev@users.noreply.github.com>
2025-02-01 12:12:14 +0100

    Refactor breakpoint indicator component location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import { BreakpointIndicator } from "~/components/shared/breakpoint-indicator";
+import { BreakpointIndicator } from "~/components/breakpoint-indicator";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
